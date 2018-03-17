a7f4bdbd914b91fbd7afbad926f7e6da9a6ed4e6
Chris-Chang <hw@yunbin.kr>
2018-03-18 01:46:17 +0900

    update to make 3rd, 4th weapon infinity too.
    update to make 3rd, 4th weapon infinity too.

--- infinity-bullet/main.cpp
+++ infinity-bullet/main.cpp
@@ -29,23 +29,35 @@ int applyCheat(HANDLE hProcess)
 	if (res[0] == 0x90) {
 		int select = MessageBox(NULL, "do you want to go back?", "1/2 Life", MB_YESNO);
 		if (select == IDYES) {
-			const LPVOID base = (LPVOID)0x2208BA43;
+			LPVOID base = (LPVOID)0x2208BA43;
 			BYTE buffer[5] = { 0x89, 0x10, 0xC2, 0x04, 0x00 };
 			BOOL is_successed = WriteProcessMemory(hProcess, base, buffer, sizeof(buffer), NULL);
-			return is_successed == 0;
+			base = (LPVOID)0x2209A5D1;
+			BYTE buffer1[6] = { 0x89, 0x9E, 0xC4, 0x04, 0x00, 0x00 };
+			is_successed = WriteProcessMemory(hProcess, base, buffer1, sizeof(buffer1), NULL);
+			base = (LPVOID)0x22143A09;
+			BYTE buffer2[7] = { 0x89, 0x9C, 0xBE, 0x30, 0x06, 0x00, 0x00 };
+			is_successed = WriteProcessMemory(hProcess, base, buffer2, sizeof(buffer2), NULL);
 		}
 
 	}
 	else {
 		int select = MessageBox(NULL, "Are you sure that you want infinity bullet?", "1/2 Life", MB_YESNO);
 		if (select == IDYES) {
-			const LPVOID base = (LPVOID)0x2208BA43;
+			LPVOID base = (LPVOID)0x2208BA43;
 			BYTE buffer[2] = { 0x90, 0x90 };
 			BOOL is_successed = WriteProcessMemory(hProcess, base, buffer, sizeof(buffer), NULL);
-			return is_successed == 0;
+			base = (LPVOID)0x2209A5D1;
+			BYTE buffer1[6] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+			is_successed = WriteProcessMemory(hProcess, base, buffer1, sizeof(buffer1), NULL);
+			base = (LPVOID)0x22143A09;
+			BYTE buffer2[7] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+			is_successed = WriteProcessMemory(hProcess, base, buffer2, sizeof(buffer2), NULL);
+
 		}
 
 	}
+	return 0;
 	
 }
 
